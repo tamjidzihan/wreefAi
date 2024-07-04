90b349391a19868f836bbfe21525e3a6ebc16253
tamjidzihan <tamjidzihan@gmail.com>
2024-07-04 17:45:24 +0600

    office 2024-07-04

--- src/components/services/ServiceItems.tsx
+++ src/components/services/ServiceItems.tsx
@@ -1,62 +1,51 @@
 import { GridItem, Text, Card, CardBody, Stack, Heading, Divider, CardFooter, ButtonGroup, Button, Box, Icon, useColorModeValue } from '@chakra-ui/react'
-import { FaUserFriends, FaMapMarkerAlt, FaSchlix, FaFirstOrderAlt, FaBrain, FaDonate, FaArrowRight } from "react-icons/fa";
-
-const items = [
-    { id: 1, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaUserFriends },
-    { id: 2, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaMapMarkerAlt },
-    { id: 3, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaSchlix },
-    { id: 4, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaFirstOrderAlt },
-    { id: 5, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaBrain },
-    { id: 6, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaDonate },
-]
+import { FaArrowRight } from "react-icons/fa";
 
 
-const ServiceItems = () => {
+interface ServiceItemProps {
+    id: number
+    icon: typeof FaArrowRight;
+    heading: string;
+    text: string
+}
+
+
+const ServiceItems = ({ id, icon, heading, text }: ServiceItemProps) => {
     return (
-        <>
-
-            {
-                items.map(item =>
-                    <GridItem
-
-                    >
-                        <Card
-                            maxW='sm'
-                            borderRadius={'xl'}
-                            bg={useColorModeValue('RGBA(0, 0, 0, 0.06)', 'RGBA(0, 0, 0, 0.36)')}
-                        >
-                            <CardBody>
-                                <Box>
-                                    <Icon
-                                        margin={3}
-                                        p={2}
-                                        borderRadius={'lg'}
-                                        fontSize='6xl'
-                                        as={item.icon}
-                                        bgGradient='linear(to-r, #0F2027, #2C5364)'
-                                    />
-                                </Box>
-                                <Stack mt='6' spacing='3'>
-                                    <Text fontSize='3xl'>{item.heading}</Text>
-                                    <Text>
-                                        {item.text}
-                                    </Text>
-                                </Stack>
-                            </CardBody>
-                            <Divider />
-                            <CardFooter>
-                                <ButtonGroup spacing='2' >
-                                    <Button variant='outline' colorScheme='purple'>
-                                        <FaArrowRight fontSize='20px' />
-                                    </Button>
-                                </ButtonGroup>
-                            </CardFooter>
-                        </Card>
-                    </GridItem>
-                )
-            }
-        </>
-
+        <GridItem>
+            <Card
+                maxW='sm'
+                borderRadius={'xl'}
+                bg={useColorModeValue('RGBA(0, 0, 0, 0.06)', 'RGBA(0, 0, 0, 0.36)')}
+            >
+                <CardBody>
+                    <Box>
+                        <Icon
+                            margin={3}
+                            p={2}
+                            borderRadius={'lg'}
+                            fontSize='6xl'
+                            as={icon}
+                            bgGradient='linear(to-r, #0F2027, #2C5364)'
+                        />
+                    </Box>
+                    <Stack mt='6' spacing='3'>
+                        <Text fontSize='3xl'>{heading}</Text>
+                        <Text>
+                            {text}
+                        </Text>
+                    </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                    <ButtonGroup spacing='2' >
+                        <Button variant='outline' colorScheme='purple'>
+                            <FaArrowRight fontSize='20px' />
+                        </Button>
+                    </ButtonGroup>
+                </CardFooter>
+            </Card>
+        </GridItem>
     )
 }
 
--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,17 +1,26 @@
-import { Box, Grid, Heading, Icon } from '@chakra-ui/react';
+import { Box, Grid, Heading, Icon, SimpleGrid } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import ServiceItems from './ServiceItems';
 import { FaServicestack } from "react-icons/fa";
+import { FaUserFriends, FaMapMarkerAlt, FaSchlix, FaFirstOrderAlt, FaBrain, FaDonate, FaArrowRight } from "react-icons/fa";
 
-const MotionBox = motion(Box);
 const MotionHeading = motion(Heading);
+
+
+const servicesItems = [
+    { id: 1, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaUserFriends },
+    { id: 2, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaMapMarkerAlt },
+    { id: 3, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaSchlix },
+    { id: 4, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaFirstOrderAlt },
+    { id: 5, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaBrain },
+    { id: 6, heading: 'Living room Sofa', text: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces', icon: FaDonate },
+]
 
 
 
 const Services = () => {
     return (
         <>
-
             <MotionHeading
                 fontSize={{ base: '5xl', lg: '6xl' }}
                 fontWeight="bold"
@@ -22,13 +31,16 @@ const Services = () => {
             </MotionHeading>
 
 
-            <Grid
-                templateColumns='repeat(3,1fr)'
+            <SimpleGrid
+                columns={{ sm: 1, md: 2, lg: 3 }}
                 gap={4}
                 mt={12}
             >
-                <ServiceItems />
-            </Grid>
+                {servicesItems.map(item =>
+                    <ServiceItems key={item.id} id={item.id} icon={item.icon} heading={item.heading} text={item.text} />
+                )}
+
+            </SimpleGrid>
         </>
     );
 };
